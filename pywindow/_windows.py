89aef02ab2a22e59324187c3fced3519aa500c5a
evfredericksen <evfredericksen@gmail.com>
2018-02-07 07:40:16 -0500

    delete dead code

--- pywindow/_windows.py
+++ pywindow/_windows.py
@@ -33,67 +33,6 @@ def maximize_active_window():
     hwnd = ctypes.windll.user32.GetForegroundWindow()
     ctypes.windll.user32.ShowWindow(hwnd, 3)
 
-def get_window_title(hwnd):
-    length = GetWindowTextLength(hwnd)
-    buff = ctypes.create_unicode_buffer(length + 1)
-    GetWindowText(hwnd, buff, length + 1)
-    return buff.value
-
-def get_matching_windows(title_list):
-    matches = {}
-
-    def window_enum_callback(hwnd, lParam):
-        if IsWindowVisible(hwnd):
-            window_name = get_window_title(hwnd).lower()
-            for name in title_list:
-                if name not in window_name:
-                    return True
-            matches[window_name] = hwnd
-        return True
-
-    EnumWindows(EnumWindowsProc(window_enum_callback), 0)
-    return matches
-
-def activate_window(title, position=1):
-    if position > 0:
-        position -= 1
-    matches = get_matching_windows(title)
-    sorted_keys = list(sorted(matches.keys(), key=len))
-    key = sorted_keys[position]
-    hwnd = matches[key]
-    # magic incantations to activate window consistently
-    IsIconic = ctypes.windll.user32.IsIconic
-    ShowWindow = ctypes.windll.user32.ShowWindow
-    GetForegroundWindow = ctypes.windll.user32.GetForegroundWindow
-    GetWindowThreadProcessId = ctypes.windll.user32.GetWindowThreadProcessId
-    BringWindowToTop = ctypes.windll.user32.BringWindowToTop
-    AttachThreadInput = ctypes.windll.user32.AttachThreadInput
-    SetForegroundWindow = ctypes.windll.user32.SetForegroundWindow
-    SystemParametersInfo = ctypes.windll.user32.SystemParametersInfoA
-    
-    if IsIconic(hwnd):
-        ShowWindow(hwnd, SW_RESTORE)
-    if GetForegroundWindow() == hwnd:
-        return True
-    ForegroundThreadID = GetWindowThreadProcessId(GetForegroundWindow(), None)
-    ThisThreadID = GetWindowThreadProcessId(hwnd, None)
-    if AttachThreadInput(ThisThreadID, ForegroundThreadID, True):
-        BringWindowToTop(hwnd)
-        SetForegroundWindow(hwnd)
-        AttachThreadInput(ThisThreadID, ForegroundThreadID, False)
-        if GetForegroundWindow() == hwnd:
-            return True
-    timeout = ctypes.c_int()
-    zero = ctypes.c_int(0)
-    SystemParametersInfo(SPI_GETFOREGROUNDLOCKTIMEOUT, 0, ctypes.byref(timeout), 0)
-    (SPI_SETFOREGROUNDLOCKTIMEOUT, 0, ctypes.byref(zero), SPIF_SENDCHANGE)
-    BringWindowToTop(hwnd)
-    SetForegroundWindow(hwnd)
-    SystemParametersInfo(SPI_SETFOREGROUNDLOCKTIMEOUT, 0, ctypes.byref(timeout), SPIF_SENDCHANGE); 
-    if GetForegroundWindow() == hwnd:
-        return True
-    return False
-
 class WindowImplementation:
 
     def __init__(self, hwnd):
